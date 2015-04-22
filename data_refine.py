13866d71a44b9d8a69205d84655db20aecc48034
WIMPIE <u12031730@tuks.co.za>
2015-04-22 19:10:10 +0200

    More robust use of refining functions
    Two methods exist to refine the YI-data:
    *Locate the maximum YI and cut all the data after that point
    *Locate the maximum YI and let all the data points after the max be
    equal to the max (it is supposed that the PVC strips cant be less
    black/degraded if it spends more time inside the metrastat
    
    Two separate functions existed for each method. They were combined into
    a single function where a boolean indicates which method will be used

--- data_refine.py
+++ data_refine.py
@@ -7,8 +7,9 @@ import os
 import pandas
 
 
-# extract all file names
+# extract all file names of a particular file type
 def get_csv(extension):
+    "file type csv"
     # access database with names of available results
     fname = set_filename(extension)
     directory = glob.glob(os.path.join(fname, '*.csv'))
@@ -17,12 +18,14 @@ def get_csv(extension):
 
 
 def get_xlsx(extension):
+    "file type xlsl"
     fname = set_filename(extension)
     directory = glob.glob(os.path.join(fname, '*.xlsx'))
 
     return directory
 
 
+# generate the file names of all the metrastat data
 def get_file_names(extension, Ca_Only=False):
     directory = get_xlsx(extension)
     results = pandas.read_excel(directory[0], 0, index_col='Date')
@@ -45,8 +48,8 @@ def get_file_names(extension, Ca_Only=False):
     return files
 
 
-# remove faulty data after max YI
-def refine_YI(raw_data):
+# remove faulty data
+def refine_YI(raw_data, extended=False):
     " raw_data must be a pandas dataframe "
 
     # find maximum YI in experimental data
@@ -55,27 +58,23 @@ def refine_YI(raw_data):
     # get time index where maximum YI occurs
     max_Index = raw_data[max_YI].index
 
-    # slice the data after maximum YI
-    trunc = raw_data.index > max_Index
+    if extended:
+        # slice the data after maximum YI
+        trunc = raw_data.index > max_Index
 
-    # update faulty data to remain at the maximum YI
-    raw_data['YI'][trunc] = raw_data.YI.max()
+        # update faulty data to remain at the maximum YI
+        raw_data['YI'][trunc] = raw_data.YI.max()
 
-    return raw_data['YI']
+        # generate refined result
+        result_YI = raw_data['YI']
+        result_time = raw_data.index
+    else:
+        # slice the data after maximum YI
+        trunc = raw_data.index < max_Index
 
+        # generate refined result
+        result_YI = raw_data[trunc]['YI']
+        result_time = raw_data[trunc].index
 
-# remove faulty data after max YI
-def refine_max(raw_data):
-    " raw_data must be a pandas dataframe "
-
-    # find maximum YI in experimental data
-    max_YI = raw_data.YI == raw_data.YI.max()
-
-    # get time index where maximum YI occurs
-    max_Index = raw_data[max_YI].index
-
-    # slice the data after maximum YI
-    trunc = raw_data.index < max_Index
-
-    return [raw_data[trunc].index,
-            raw_data[trunc]['YI']]
+    return [result_YI,
+            result_time]
